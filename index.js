da246e496506b922292fa3fc4b3a26b3062effbb
Ayan Ur Rehman Khan <ayanurehmank@gmail.com>
2024-12-25 17:52:55 +0500

    Only Operate Addition

---
+++ index.js
@@ -0,0 +1,7 @@
+alert("This is a Simple Calculator.It only works on Addition Operator.");
+
+var num1 = +prompt("Enter your First Value:-");
+var num2 = +prompt("Enter your Second Value");
+
+var result = num1 + num2;
+alert("Your result is"+ result);
